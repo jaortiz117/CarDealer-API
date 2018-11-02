2a9546b07687029b60619cde798ef322e9334a31
JavierO <javier.ortiz13@upr.edu>
2018-11-02 11:39:48 -0400

    added day constants

--- src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
@@ -8,12 +8,19 @@ import edu.uprm.cse.datastructures.cardealer.util.LinkedPositionalList;
 import edu.uprm.cse.datastructures.cardealer.util.interfaces.PositionalList;
 
 public class AppointmentList {
+	public static final int MONDAY = 0; 
+	public static final int TUESDAY = 1; 
+	public static final int WEDNESDAY = 2;
+	public static final int THURSDAY = 3; 
+	public static final int FRIDAY = 4;
+	
 	private static final int WEEK = 5;
 	
 	private final static PositionalList<Appointment>[] appointmentsList = generateList(WEEK, new LinkedPositionalList<Appointment>());
 
 	private AppointmentList(){}
 
+	@SuppressWarnings("unchecked")
 	private static LinkedPositionalList<Appointment>[] generateList(int size, LinkedPositionalList<Appointment> list) {
 		LinkedPositionalList<Appointment>[] arr = (LinkedPositionalList<Appointment>[]) Array.newInstance(list.getClass(), size);
 		for (int i = 0; i < arr.length; i++) {
