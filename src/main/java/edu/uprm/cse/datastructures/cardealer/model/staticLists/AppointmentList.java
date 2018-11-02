13d66d3c125bef84a26e348be88c46115ba3ea55
JavierO <javier.ortiz13@upr.edu>
2018-11-02 11:11:32 -0400

    appointment list factory tested

--- src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
@@ -1,23 +1,25 @@
 package edu.uprm.cse.datastructures.cardealer.model.staticLists;
 
 
+import java.lang.reflect.Array;
+
 import edu.uprm.cse.datastructures.cardealer.model.Appointment;
 import edu.uprm.cse.datastructures.cardealer.util.LinkedPositionalList;
 import edu.uprm.cse.datastructures.cardealer.util.interfaces.PositionalList;
 
 public class AppointmentList {
 	private static final int WEEK = 5;
-	private final static PositionalList<Appointment>[] appointmentsList = generateList(WEEK);
+	
+	private final static PositionalList<Appointment>[] appointmentsList = generateList(WEEK, new LinkedPositionalList<Appointment>());
 
 	private AppointmentList(){}
 
-	@SuppressWarnings("unchecked")
-	private static LinkedPositionalList<Appointment>[] generateList(int size) {
-		Object[] arr = new Object[size];
+	private static LinkedPositionalList<Appointment>[] generateList(int size, LinkedPositionalList<Appointment> list) {
+		LinkedPositionalList<Appointment>[] arr = (LinkedPositionalList<Appointment>[]) Array.newInstance(list.getClass(), size);
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = new LinkedPositionalList<Appointment>();
 		}
-		return  (LinkedPositionalList<Appointment>[]) arr;
+		return  arr;
 	}
 
 	public static PositionalList<Appointment>[] getInstance(){
