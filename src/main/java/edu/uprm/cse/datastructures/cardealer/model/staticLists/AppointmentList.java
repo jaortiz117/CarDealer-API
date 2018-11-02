c0c71822ce0090c83e73a2aa1fa349827f6089a5
JavierO <javier.ortiz13@upr.edu>
2018-11-01 20:45:23 -0400

    fixed some static appointment list issues

--- src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/staticLists/AppointmentList.java
@@ -7,14 +7,17 @@ import edu.uprm.cse.datastructures.cardealer.util.interfaces.PositionalList;
 
 public class AppointmentList {
 	private static final int WEEK = 5;
-	private final static PositionalList<Appointment>[] appointmentsList = generateList(WEEK, 
-			new LinkedPositionalList<Appointment>());
+	private final static PositionalList<Appointment>[] appointmentsList = generateList(WEEK);
 
 	private AppointmentList(){}
 
-	private static <E> E[] generateList(int size, E list) {
-		E[] arr = (E[])new Object[size];
-		return arr;
+	@SuppressWarnings("unchecked")
+	private static LinkedPositionalList<Appointment>[] generateList(int size) {
+		Object[] arr = new Object[size];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = new LinkedPositionalList<Appointment>();
+		}
+		return  (LinkedPositionalList<Appointment>[]) arr;
 	}
 
 	public static PositionalList<Appointment>[] getInstance(){
