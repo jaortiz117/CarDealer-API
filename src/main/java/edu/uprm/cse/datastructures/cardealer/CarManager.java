da09a3918fba6de4fa4ae1e1acd6f6132f278ba0
JavierO <javier.ortiz13@upr.edu>
2018-11-07 23:02:33 -0400

    added year search method

--- src/main/java/edu/uprm/cse/datastructures/cardealer/CarManager.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/CarManager.java
@@ -47,7 +47,7 @@ public class CarManager {
 				return car;
 		}
 
-//		throw new NotFoundException(new JsonError("Error", "Car " + id + " not found"));
+		//		throw new NotFoundException(new JsonError("Error", "Car " + id + " not found"));
 		throw new NotFoundException();
 	}
 
@@ -63,7 +63,7 @@ public class CarManager {
 	}
 
 	@PUT
-    @Path("/{id}/update")
+	@Path("/{id}/update")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response updateCar(@PathParam("id") long id, Car car){
 		if(car.getCarId() != id)
@@ -81,8 +81,8 @@ public class CarManager {
 	}
 
 	@DELETE
-    @Path("/{id}/delete")
-    public Response deleteCar(@PathParam("id") long id){
+	@Path("/{id}/delete")
+	public Response deleteCar(@PathParam("id") long id){
 		for(Car car: carList) {
 			if(car.getCarId() == id) {
 				if(carList.remove(car))
@@ -90,14 +90,14 @@ public class CarManager {
 			}
 		}
 		return Response.status(Response.Status.NOT_FOUND).build();
-    }
-	
+	}
+
 	@GET
 	@Path("/brand/{brandName}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Car[] brandSearch(@PathParam("brandName")String brand) {
 		ArrayList<Car> result = new ArrayList<>();
-		
+
 		//since it is sorted by brand names first, when we reach another brand 
 		//we can just return the final list
 		//If we reach end of list and result length is 0 throw 404
@@ -107,10 +107,26 @@ public class CarManager {
 			else if(!result.isEmpty())
 				return result.toArray(new Car[result.size()]);
 		}
-		
+
 		//if list is empty we throw 404, else we return list
 		if(!result.isEmpty()) return result.toArray(new Car[result.size()]);
-		
+
+		throw new NotFoundException();
+	}
+
+	@GET
+	@Path("/year/{year}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Car[] yearSearch(@PathParam("year") long year) {
+		ArrayList<Car> result = new ArrayList<>();
+
+		for(Car car : carList) {
+			if(car.getCarYear() == year)
+				result.add(car);
+		}
+
+		if(!result.isEmpty()) return result.toArray(new Car[result.size()]);
+
 		throw new NotFoundException();
 	}
 }
